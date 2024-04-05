686b1fc98e6ae672461ca51018d0ba6ea61d1fc8
VarshithPawarHR <varshithpawarhr@gmail.com>
2024-04-06 01:43:12 +0530

    updated styles

--- src/routes/auth/login/+page.svelte
+++ src/routes/auth/login/+page.svelte
@@ -12,38 +12,28 @@
 
 <div class="container" id="container">
     <div class="form-container sign-up">
-        <form>
-            <h1>Create Account</h1>
-            
-            <span>or use your email for registeration</span>
-            <input type="text" placeholder="Name">
-            <input type="email" placeholder="Email">
-            <input type="password" placeholder="Password">
-            <button>Sign Up</button>
-        </form>
+        
     </div>
     <div class="form-container sign-in">
         <form>
             <h1>Sign In</h1>
            
             
-            <input type="email" placeholder="Email">
-            <input type="password" placeholder="Password">
-            <a href="#">→ Forget Your Password? ←</a>
+            <input type="text" name="username" required />
+    <input type="password" name="password" required />
+            <a href="#">→ Let's contribute to our city ←</a>
             <button>Sign In</button>
         </form>
     </div>
     <div class="toggle-container">
         <div class="toggle">
             <div class="toggle-panel toggle-left">
-                <h1>Welcome Back!</h1>
-                <p>Enter your personal details to use all of site features</p>
-                <button class="hidden" id="login">Sign In</button>
+                
             </div>
             <div class="toggle-panel toggle-right">
-                <h1>Hello, Friend!</h1>
-                <p>Register with your personal details to use all of site features</p>
-                <button class="hidden" id="register">Sign Up</button>
+                <h1>Not Registered yet??</h1>
+                <p>Register with your Aadhaar to join the community</p>
+                <button style= "background-color:#1E293B">Sign Up</button>
             </div>
         </div>
     </div>
@@ -94,7 +84,7 @@
     }
 
     .container button {
-        background-color: #512da8;
+        background-color: #334155;
         color: #fff;
         font-size: 12px;
         padding: 10px 45px;
@@ -199,7 +189,7 @@
     }
 
     .toggle {
-        background-color: #512da8;
+        background-color: #334155;
         height: 100%;
         color: #fff;
         position: relative;
