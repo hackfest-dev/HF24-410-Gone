0d5e9c4ce0d3c37e5f9819691b09446fab6969e6
Karthik-S-Salian <karthikssalian5@gmail.com>
2024-04-06 11:38:45 +0530

    added map

---
+++ src/routes/(main)/map/+page.svelte
@@ -0,0 +1,26 @@
+<script>
+    import "leaflet/dist/leaflet.css";
+    //@ts-ignore
+    import { Map, TileLayer, Marker, Popup } from "svelte-map-leaflet";
+
+    const mapOptions = { center: [40.6852119, -74.0788838], zoom: 10 };
+    const tileUrl = "https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png";
+    const markerLatLng = [40.6852119, -74.0788838];
+    const popupMessage = "Statue of Liberty National Monument";
+</script>
+
+<div class="map">
+    <Map options={mapOptions}>
+        <TileLayer url={tileUrl}></TileLayer>
+        <Marker latLng={markerLatLng}>
+            <Popup>{popupMessage}</Popup>
+        </Marker>
+    </Map>
+</div>
+
+<style>
+    .map {
+        height: 400px;
+        width: 400px;
+    }
+</style>
